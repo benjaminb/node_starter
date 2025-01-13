4565b775cdab133bbb3dc5a539323e5800ab5b09
benjaminb <benbasseri@gmail.com>
2025-01-13 13:12:15 -0500

    adds error controller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 // route imports
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/errors');
 
 // template engine setup
 const app = express();
@@ -19,8 +20,6 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 // catchall route
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
