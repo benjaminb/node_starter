9e764a7c1843398fd9d39b3a0067db35a77318e5
benjaminb <benbasseri@gmail.com>
2025-01-15 13:08:28 -0500

    factors out shop/admin views and controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     activeAddProduct: true,
@@ -19,12 +19,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render('shop', {
+    res.render('admin/products', {
       products: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShope: true,
+      path: '/admin/products',
+      pageTitle: 'Admin Products',
     });
   });
 };
---
+++ controllers/shop.js
@@ -0,0 +1,37 @@
+const Product = require('../models/product');
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    pageTitle: 'Your Cart',
+    path: '/shop/cart',
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    pageTitle: 'Checkout',
+    path: '/checkout',
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/index', {
+      products: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShope: true,
+    });
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/product-list', {
+      products: products,
+      pageTitle: 'All Products',
+      path: '/product-list',
+    });
+  });
+};
